c3a721a7850b903b7abf03c097828940dfe11727
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-11-23 15:47:20 +0100

    Update to rc10

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup_args = dict(
     cmdclass= cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyterlab>=3.0.0rc8,==3.*",
+        "jupyterlab>=3.0.0rc10,==3.*",
         "jupyterlab-topbar>=0.6.0,<0.7.0",
         "nbresuse>=0.4.0,<0.5.0"
     ],
