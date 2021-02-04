7969c951a462e71f4188c5860ad108ba2c9b5073
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-02-04 22:17:12 +0100

    Depend on jupyter-resource-usage

--- setup.py
+++ setup.py
@@ -62,9 +62,9 @@ setup_args = dict(
     cmdclass= cmdclass,
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyterlab>=3.0.0rc10,==3.*",
-        "jupyterlab-topbar>=0.6.0,<0.7.0",
-        "nbresuse>=0.4.0,<0.5.0"
+        "jupyterlab~=3.0",
+        "jupyterlab-topbar~=0.6",
+        "jupyter-resource-usage~=0.5"
     ],
     zip_safe=False,
     include_package_data=True,
